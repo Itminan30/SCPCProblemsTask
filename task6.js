16809203e15c53d906a15cca4777629c479f3f61
Itminan30 <itminan30@gmail.com>
2023-07-27 20:16:59 +0600

    task 6 done

--- task6.js
+++ task6.js
@@ -1,8 +1,28 @@
-function passwordGenerator(len){
+function passwordGenerator(len) {
     const upperAlpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     const lowerAlpha = "abcdefghijklmnopqrstuvwxyz"
     const nums = "0123456789";
     const chars = "!@#$%^&*";
 
-    
+    const mixedChars = upperAlpha + lowerAlpha + nums + chars;
+
+    let password = "";
+    if (len >= 4) {
+        password = password + upperAlpha.charAt(Math.floor(Math.random() * (upperAlpha.length)));
+        password = password + lowerAlpha.charAt(Math.floor(Math.random() * (lowerAlpha.length)));
+        password = password + nums.charAt(Math.floor(Math.random() * (nums.length)));
+        password = password + chars.charAt(Math.floor(Math.random() * (chars.length)));
+        for (i = 4; i < len; i++) {
+            password = password + mixedChars.charAt(Math.floor(Math.random() * (mixedChars.length)))
+        }
+        console.log(password);
+    }
+    else {
+        for (i = 0; i < len; i++) {
+            password = password + mixedChars.charAt(Math.floor(Math.random() * (mixedChars.length)))
+        }
+        console.log(password);
+    }
 }
+
+passwordGenerator(8)
